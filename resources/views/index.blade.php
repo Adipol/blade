8cf67b15b25e5376793d85a5e3604dc8ee60eaf9
george <adipol13@gmail.com>
2023-04-22 20:01:12 -0400

    blade inicio

---
+++ resources/views/index.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Ejemplo de Class en HTML5</title>
+    <style>
+        .rojo {
+            color: red;
+        }
+
+        .negrita {
+            font-weight: bold;
+        }
+
+    </style>
+</head>
+
+<body>
+    <h1 class="rojo negrita">Este es un título con clases</h1>
+    <p>Este es un párrafo normal.</p>
+    <p class="rojo">Este es un párrafo con texto en rojo.</p>
+    <p claggss="negrita">Este es un párrafo con texto en negrita.</p>
+</body>
+
+</html>
